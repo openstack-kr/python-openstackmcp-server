81e3cc924f96515edfc557ac941eb6d3bb147892
S0okJu <damekyung98@gmail.com>
2025-08-08 17:40:42 +0900

    feat: Register neutron(#26)

--- src/openstack_mcp_server/tools/__init__.py
+++ src/openstack_mcp_server/tools/__init__.py
@@ -9,7 +9,9 @@ def register_tool(mcp: FastMCP):
     from .compute_tools import ComputeTools
     from .identity_tools import IdentityTools
     from .image_tools import ImageTools
+    from .neutron_tools import NeutronTools
 
     ComputeTools().register_tools(mcp)
     ImageTools().register_tools(mcp)
     IdentityTools().register_tools(mcp)
+    NeutronTools().register_tools(mcp)
