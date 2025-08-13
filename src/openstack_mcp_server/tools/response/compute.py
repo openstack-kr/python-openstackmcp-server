079f7917ec2fd6b51f8d61e8f8e7fcea0e60f713
choieastsea <choieastsea@gmail.com>
2025-08-13 20:57:36 +0900

    feat: get flavors

--- src/openstack_mcp_server/tools/response/compute.py
+++ src/openstack_mcp_server/tools/response/compute.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class Flavor(BaseModel):
+class ServerFlavor(BaseModel):
     id: str | None = Field(default=None, exclude=True)
     name: str = Field(validation_alias="original_name")
     model_config = ConfigDict(validate_by_name=True)
 
 
-class Image(BaseModel):
+class ServerImage(BaseModel):
     id: str
 
 
@@ -27,8 +27,20 @@ class Server(BaseModel):
     id: str
     name: str
     status: str | None = None
-    flavor: Flavor | None = None
-    image: Image | None = None
+    flavor: ServerFlavor | None = None
+    image: ServerImage | None = None
     addresses: dict[str, list[ServerIp]] | None = None
     key_name: str | None = None
     security_groups: list[ServerSecurityGroup] | None = None
+
+
+class Flavor(BaseModel):
+    id: str
+    name: str
+    vcpus: int
+    ram: int
+    disk: int
+    swap: int | None = None
+    is_public: bool = Field(validation_alias="os-flavor-access:is_public")
+
+    model_config = ConfigDict(validate_by_name=True)
